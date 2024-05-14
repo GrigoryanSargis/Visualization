5ad734885d34205e738de198fb9a2db5cdaa3c48
Sargis Grigoryan <79859279+GrigoryanSargis@users.noreply.github.com>
2024-05-14 21:27:37 +0400

    Update dash_with_buttoms.py

--- dash_with_buttoms.py
+++ dash_with_buttoms.py
@@ -1,7 +1,6 @@
 import plotly.express as px
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
 from dash.dependencies import Input, Output,State
 import pandas as pd
 #dataset
@@ -128,4 +127,4 @@ def add_social_buttons(figure):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host= '0.0.0.0',port=8090,debug=True)
