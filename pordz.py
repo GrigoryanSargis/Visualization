ef00b814d50d1acdbb0c50b49f5f8ffc5ce3e71c
Sargis Grigoryan <79859279+GrigoryanSargis@users.noreply.github.com>
2024-05-30 01:36:19 +0400

    Update pordz.py

--- pordz.py
+++ pordz.py
@@ -151,9 +151,5 @@ def ages_groups_page():
         ]
     )
 
-# Ensure the server can be accessed externally if needed
-server = app.server
-
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+# Expose the server
 server = app.server
